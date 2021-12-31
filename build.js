8e9c517f027705588f665b494c836d108ca56e30
YamaD <me@dyama.net>
2021-12-31 21:57:44 +0900

    あ

--- build.js
+++ build.js
@@ -2,37 +2,26 @@ const fs = require("fs")
 const glob = require("tiny-glob")
 const fm = require("front-matter")
 
+async function proc(srcDir, distFile) {
+  const files = await glob(`${srcDir}/**/*.md`)
+  const data = { data: [], }
+  files.forEach(file => {
+    const content = fm(fs.readFileSync(file).toString())
+    data.data.push({
+      slug: content.attributes.slug || file.slice(`${srcDir}/`.length, -3),
+      body: content.body,
+      ...{
+        title: content.attributes.title,
+        date: content.attributes.date,
+        dateUpd: content.attributes.dateUpd,
+      }
+    })
+  })
+  fs.writeFileSync(distFile, JSON.stringify(data))
+}
+
 const srcDir = "src"
 const distDir = "dist/api"
 
-;(async () => {
-
-  { // blog
-    const files = await glob(`${srcDir}/blog/**/*.md`)
-    const data = { data: [], }
-    files.forEach(file => {
-      const content = fm(fs.readFileSync(file).toString())
-      data.data.push({
-        slug: content.attributes.slug || file.slice(`${srcDir}/blog/`.length, -3),
-        date: content.attributes.date,
-        body: content.body,
-      })
-    })
-    fs.writeFileSync(`${distDir}/blog.json`, JSON.stringify(data))
-  }
-
-  { // news
-    const files = await glob(`${srcDir}/news/**/*.md`)
-    const data = { data: [], }
-    files.forEach(file => {
-      const content = fm(fs.readFileSync(file).toString())
-      data.data.push({
-        slug: content.attributes.slug || file.slice(`${srcDir}/blog/`.length, -3),
-        date: content.attributes.date,
-        body: content.body,
-      })
-    })
-    fs.writeFileSync(`${distDir}/news.json`, JSON.stringify(data))
-  }
-
-})()
+proc(`${srcDir}/blog`, `${distDir}/blog.json`)
+proc(`${srcDir}/news`, `${distDir}/news.json`)
