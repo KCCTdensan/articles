040cf5e07bbb41dccb4453837de090ae85ae2389
YamaD <me@dyama.net>
2021-12-31 22:08:47 +0900

    サイトの方のAPIに準拠するようにした

--- build.js
+++ build.js
@@ -8,13 +8,14 @@ async function proc(srcDir, distFile) {
   files.forEach(file => {
     const content = fm(fs.readFileSync(file).toString())
     data.data.push({
-      slug: content.attributes.slug || file.slice(`${srcDir}/`.length, -3),
-      body: content.body,
-      ...{
-        title: content.attributes.title,
-        date: content.attributes.date,
-        dateUpd: content.attributes.dateUpd,
-      }
+      slug:     content.attributes.slug || file.slice(`${srcDir}/`.length, -3),
+      title:    content.attributes.title,
+      description: content.attributes.description,
+      author:   content.attributes.author,
+      noRobots: content.attributes.noRobots,
+      date:     content.attributes.date,
+      dateUpd:  content.attributes.dateUpd,
+      body:     content.body,
     })
   })
   fs.writeFileSync(distFile, JSON.stringify(data))
